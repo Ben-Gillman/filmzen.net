d4ed8d4fce5c3ae966d784ff5fd1a130790d820a
Ben-Gillman <benjamin.o.gillman@gmail.com>
2017-09-17 22:49:33 -0400

    initial data analysis

---
+++ movie_recommendation_engine.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 19:55:15 2017
+
+@author: Ben
+"""
+
+import pandas as pd
+
+directory = r"C:\Users\Ben\Documents\Data Set\movie reviews data sets\\" 
+data = pd.read_csv(directory + "ratings.csv")
