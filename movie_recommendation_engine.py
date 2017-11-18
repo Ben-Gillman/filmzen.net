199390a13ab413f8668c4643f532be7a03220d4a
Ben-Gillman <benjamin.o.gillman@gmail.com>
2017-11-18 16:36:19 -0500

    set up recommendation engin to accept movies from input and modify database

--- movie_recommendation_engine.py
+++ movie_recommendation_engine.py
@@ -6,6 +6,65 @@ Created on Fri Sep  8 19:55:15 2017
 """
 
 import pandas as pd
+import sqlite3
+import time
 
-directory = r"C:\Users\Ben\Documents\Data Set\movie reviews data sets\\" 
-data = pd.read_csv(directory + "ratings.csv")
+def rating_similarity(movie):
+    sql_string = "select userId from ratings "\
+                 "where liked = 1 and movieId = {};".format(movie)
+    liked_movie_users = pd.read_sql_query(sql_string, con)
+    liked_movie_users.to_sql("liked_users", con, if_exists="replace", index=False)
+    
+    sql_string = "select ratings.movieId, ratings.rating from ratings "\
+                 "inner join liked_users on liked_users.userId = ratings.userId "\
+                 "where ratings.movieId != {};".format(movie)
+    liked_movie_rated = pd.read_sql_query(sql_string, con)
+    
+    liked_movie_rated = liked_movie_rated.groupby('movieId')['rating']\
+                                         .agg(['mean','count'])\
+                                         .reset_index()
+    liked_movie_rated['likedMovie'] = movie
+    cols = ['ratedMovie', 'avgRating', 'countRating', 'likedMovie']
+    liked_movie_rated = liked_movie_rated.loc[liked_movie_rated['count'] > 30, :]
+    liked_movie_rated.columns = cols
+    return liked_movie_rated
+
+
+def get_genomes(movie):
+    sql_string = "select * from genome_similarity where movieId2 = {};".format(movie)
+    return pd.read_sql_query(sql_string, con)
+
+
+def calculate_scores(ratings, genomes, movies_master):
+    scores = pd.merge(ratings, genomes, how='left', 
+                      left_on=['ratedMovie', 'likedMovie'], 
+                      right_on=['movieId', 'movieId2'])
+    
+    scores['score'] = scores.avgRating / (scores.avgRating.max() * 10) + scores.similarity
+    max_score = scores['score'].max()
+    scores['score'] = max_score - scores['score']
+    scores = pd.merge(scores, movies_master[['title', 'movieId']], 
+                      left_on='ratedMovie', right_on='movieId')
+    return scores[["likedMovie", "ratedMovie", "title", "avgRating", 
+                     "countRating", "similarity", "score"]]
+
+if __name__ == '__main__':
+    con = sqlite3.connect("C:\\Users\Ben\Documents\movie_recs\data.sqlite3")
+    movies_master = pd.read_sql_query("select * from movie_master;", con)
+    
+    # Get a movie id from user
+    movie = 1
+
+    start_time = time.time()
+    liked_rated = rating_similarity(movie)
+    print("This cell took", (time.time() - start_time) / 60, "minutes to run")
+
+    start_time = time.time()    
+    genome_similarity = get_genomes(movie)
+    print("This cell took", (time.time() - start_time) / 60, "minutes to run")
+
+    start_time = time.time()    
+    scores = calculate_scores(liked_rated, genome_similarity, movies_master)
+    print("This cell took", (time.time() - start_time) / 60, "minutes to run")
+    
+    
