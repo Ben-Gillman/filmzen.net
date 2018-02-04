3779651418fb2c3b9467bccfa4058dc609794d51
Ben-Gillman <benjamin.o.gillman@gmail.com>
2018-02-04 10:02:01 -0500

    remove results when loading image is present

--- static/movie.js
+++ static/movie.js
@@ -1,9 +1,11 @@
 const loaded = () => {
     let loadingGif = document.querySelector('.loading-gif');
+    let recContent = document.querySelector('.recommendation')
     let submitBtn = document.querySelector('#submit');
 
     submitBtn.addEventListener('click', event => {
-        loadingGif.classList.add('is-submitted')
+        loadingGif.classList.add('is-submitted');
+        recContent.classList.add('new-rec');
     })
 }
 
