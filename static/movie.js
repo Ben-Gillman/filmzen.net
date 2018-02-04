2b77ed2a7107375b3106e90430524392c39398c5
Ben-Gillman <benjamin.o.gillman@gmail.com>
2018-02-04 14:37:09 -0500

    fixed non-display error

--- static/movie.js
+++ static/movie.js
@@ -1,11 +1,13 @@
 const loaded = () => {
     let loadingGif = document.querySelector('.loading-gif');
     let recContent = document.querySelector('.recommendation');
+    let recIntro = document.querySelector('.recommendation-intro');
     let submitBtn = document.querySelector('#submitMovie');
 
     submitBtn.addEventListener('click', event => {
         loadingGif.classList.add('is-submitted');
         recContent.classList.add('new-rec');
+        recIntro.classList.add('new-rec');
     })
 }
 
